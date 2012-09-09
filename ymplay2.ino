02b2e219b3c22712af702bc86fa5f347fc99f7a0
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2012-09-09 17:25:45 +0200

    Properly parse header, sort of, and handle looping.

--- ymplay2.ino
+++ ymplay2.ino
@@ -61,9 +61,73 @@ void write_reg(byte reg, byte value) {
   psg_inactive();
 }
 
+
+File ymFile;
+
+char title[21];
+char author[21];
+char convertor[21];
 byte buffer[128];
 volatile byte sdpos = 0;
 volatile byte psgpos = 0;
+int frames = 0;
+int frames_loaded = 0;
+int loop_frame = 0;
+int data_start = 0;
+
+static inline uint8_t read8() {
+  if(ymFile.available()) {
+    return ymFile.read();
+  }
+  return 0;
+}
+
+static inline uint16_t read16() {
+  return (read8()<<8)|read8();
+}
+
+static inline uint32_t read32() {
+  return (read16()<<16)|read16();
+}
+
+static inline void readstr(char *ptr, int maxsize) {
+  int pos = 0;
+  byte tmp = 0xff;
+  while(tmp != 0x00) {
+    tmp = ymFile.read();
+    if(pos < maxsize) {
+      ptr[pos] = tmp;
+      pos++;
+    }
+  }
+  if(pos == maxsize) {
+    ptr[pos] = '\0';
+  }
+}
+
+void read_header() {
+  // Skip first bits
+  ymFile.seek(12);
+  frames = read32();
+  ymFile.seek(28);
+  loop_frame = read32();
+  ymFile.seek(34);
+  readstr(title, sizeof(title)-1);
+  readstr(author, sizeof(author)-1);
+  readstr(convertor, sizeof(convertor)-1);
+  data_start = ymFile.position();
+  Serial.print("Title: ");
+  Serial.println(title);
+  Serial.print("Author: ");
+  Serial.println(author);
+  Serial.print("Convertor: ");
+  Serial.println(convertor);
+  Serial.print("Length: ");
+  Serial.print(frames/50);
+  Serial.println(" seconds");
+  Serial.print("Loop at: ");
+  Serial.println(loop_frame);
+}
 
 
 ISR(TIMER1_COMPA_vect)
@@ -75,8 +139,6 @@ ISR(TIMER1_COMPA_vect)
   psgpos &= 0x7f;
 }
 
-File ymFile;
-
 void setup() {
   for(int i=2;i<=9;i++) {
     pinMode(i, OUTPUT);
@@ -84,9 +146,6 @@ void setup() {
   pinMode(psgbc1, OUTPUT);
   pinMode(psgbdir, OUTPUT);
   pinMode(CS, OUTPUT);
-  /*  pinMode(MOSI, INPUT);
-  pinMode(MISO, OUTPUT);
-  pinMode(SCK, OUTPUT);*/
 
   psg_inactive();
   write_reg(7, 255);
@@ -96,8 +155,10 @@ void setup() {
   Serial.begin(9600);
 
   pinMode(13, OUTPUT);
+
+  /* Set timer to 50Hz */
   cli();
-  TCCR1A = 0;        // set entire TCCR1A register to 0
+  TCCR1A = 0;
   TCCR1B = 0;
   OCR1A = 312;
   TCCR1B |= (1 << WGM12);
@@ -106,6 +167,7 @@ void setup() {
   TIMSK1 |= (1 << OCIE1A);
   sei();
 
+  
   if (!SD.begin(CS)) {
     Serial.println("initialization failed!");
     return;
@@ -116,11 +178,8 @@ void setup() {
     Serial.println("Unable to open file");
     return;
   }
-  for(int i=0;i<106;i++) {
-    if(ymFile.available()) {
-      ymFile.read();
-    }
-  }
+
+  read_header();
 }
 
 void read_frame() {
@@ -131,6 +190,11 @@ void read_frame() {
       sdpos &= 0x7f;
     }
   }
+  frames_loaded++;
+  if(frames_loaded >= frames) {
+    frames_loaded = loop_frame;
+    ymFile.seek(data_start+16*loop_frame);
+  }
 }
 
 
@@ -138,19 +202,4 @@ void loop() {
   while(((sdpos+128-psgpos)&0x7f) < 64) {
     read_frame();
   }
-
-//  digitalWrite(13, HIGH);
-//  PORTB=(1<<5);
-//  write_reg(7, 0b00111110);
-//  PORTB=0;
-//  digitalWrite(13, LOW);
-//  write_reg(8, 15);
-//  write_reg(0, count);
-//  write_reg(1, 0);
-//  count += 1;
-//  delay(10);
-//  if(count == 0) {
-//    Serial.println("Looping...");
-//  }
-  
 }
